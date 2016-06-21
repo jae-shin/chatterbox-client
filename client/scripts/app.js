c3bab4b72145d561ec6e783bfb0ce77b4d01d6cf
jae-shin <jae5464@gmail.com>
2016-06-21 12:18:04 -0700

    Refactor to pass spec up until chatroom behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,64 @@
-//Global object to house all roomnames
-var rooms = {all: 'all'};
-var friends = [];
+const app = {};
 
-var getMessages = function() {
+app.init = function() {
+  this._rooms = {all: 'all'};
+  this._friends = [];
+  this.server = 'https://api.parse.com/1/classes/messages';
+  $(() => {
+    $('#post-button').on('click', function() {
+      var $username = $('input[name="username"]');
+      var $text = $('textarea[name="message"]');
+      var $roomname = $('input[name="roomname"]');
+
+      // TODO: if username or text is empty string, throw a red error
+
+      var roomname = $roomname.val() || $('#roomSelect').val();
+      
+      var message = {
+        username: $username.val(), 
+        text: $text.val(),
+        roomname: roomname
+      };
+
+      app.send(message);
+
+      // Clear the text in the input fields:
+      $username.val('');
+      $text.val('');
+      $roomname.val('');
+    });
+
+    $('#roomSelect').on('change', app._refreshMessages);
+
+    $('#chats').on('click', '.username', function() {    
+      app._friends.push($(this).text());
+      app.fetch();
+    });
+
+    setInterval(app._refreshMessages, 5000);
+
+    app.fetch();
+  });
+};
+
+app.fetch = function(roomname) {
+  var dataObject = {};
+
+  if (roomname) {
+    dataObject.where = {'roomname': roomname};
+  } else {
+    dataObject.format = 'json';
+  }
+
   $.ajax({
-    //some comment
-    url: 'https://api.parse.com/1/classes/messages',
+    url: this.server,
     type: 'GET',
-    data: {
-      format: 'json'
-    },
+    data: dataObject,
     contentType: 'application/json',
     success: function(data) {
       // console.log('chatterbox: messages retrieved');
-      updateRooms(data);
-      displayMessages(data);
+      app._updateRooms(data);
+      app._displayMessages(data);
     },
     error: function(data) {
       console.error('chatterbox: failed to retrieve messages', data);
@@ -22,42 +66,36 @@ var getMessages = function() {
   });
 };
 
-var displayMessages = function(data) {
-  $('.chat').remove();
-  var results = data.results;
-  results.forEach(function(message) {    
-
-    var $message = $('<div class="chat"></div>');
-    var $username = $('<span class="username"></span>');
-    $username.text(message.username);
-
-    if (friends.indexOf(message.username) > -1) {
-      $message.addClass('friend');
-    }
-
-    var $text = $('<span></span>');
-    $text.text(': ' + message.text);
-
-    $message.append($username);
-    $message.append($text);
-
-    if (message.roomname) {
-      $message.attr('room', message.roomname);
-    } else {
-      $message.attr('room', 'all');
-    }
-    
-    $('#chats').append($message);
-
-    //Hide messages if the room selected does not match
-    if ($('#room-selector').val() !== 'all' &&
-        $('#room-selector').val() !== message.roomname) {
-      $message.hide();
-    }
-  });
+app._displayMessages = function(data) {
+  app.clearMessages();
+  data.results.forEach(message => app.addMessage(message));
 };
 
-var postMessage = function(message) {
+app.addMessage = function(message) {
+  var $message = $('<div class="chat"></div>');
+  var $username = $('<span class="username"></span>');
+  $username.text(message.username);
+
+  if (this._friends.indexOf(message.username) > -1) {
+    $message.addClass('friend');
+  }
+
+  var $text = $('<span></span>');
+  $text.text(': ' + message.text);
+
+  $message.append($username);
+  $message.append($text);
+
+  if (message.roomname) {
+    $message.attr('room', message.roomname);
+  } else {
+    $message.attr('room', 'all');
+  }
+  
+  $('#chats').append($message); 
+};
+
+app.send = function(message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
@@ -66,7 +104,8 @@ var postMessage = function(message) {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
-      getMessages();
+      //Needs to be refresh messages
+      app._refreshMessages();
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -75,83 +114,40 @@ var postMessage = function(message) {
   });
 };
 
-
-var updateRooms = function(data) {
-
+app._updateRooms = function(data) {
   var results = data.results;
-
-  results.forEach(function(message) {
-
+  results.forEach(message => {
     //Check if the message has a roomname
     //AND check if the room name doesn't already exist in rooms object
-    if (message.roomname && !rooms[message.roomname]) {
-      //Add room to global rooms object
-      rooms[message.roomname] = message.roomname;
-      //Update selector with room name
-      $('#room-selector').append($('<option>', { 
-        value: message.roomname,
-        text: message.roomname 
-      }));
+    if (message.roomname && !this._rooms[message.roomname]) {
+      app.addRoom(message.roomname);
     }
-
   });
 };
 
+app.addRoom = function(roomname) {
+  this._rooms[roomname] = roomname;
+  //Update selector with room name
+  $('#roomSelect').append($('<option>', { 
+    value: roomname,
+    text: roomname 
+  }));
+};
 
-$(document).ready(function() {
-  //var $refreshButton = $('<button id=refresh>Refresh!</button>');
-  //$('#main').append($refreshButton);
+app._refreshMessages = function() {
+  var $roomSelector = $('#roomSelect');
+  if ($roomSelector.val() === 'all') {
+    app.fetch();
+  } else {
+    app.fetch($roomSelector.val());
+  }
+};
 
-  // $refreshButton.on('click', function() {
-  //   getMessages();
-  // });
+app.clearMessages = function() {
+  $('#chats').empty();
+};
 
-  setInterval(function() {
-    getMessages();
-  }, 5000);
-
-  var $postButton = $('#post');
-  $postButton.on('click', function() {
-    var $username = $('input[name="username"]');
-    var $text = $('textarea[name="message"]');
-    var $roomname = $('input[name="roomname"]');
-
-    // TODO: if username or text is empty string, through a red error
-
-    var message = {
-      username: $username.val(), 
-      text: $text.val(),
-      roomname: $roomname.val()
-    };
-    postMessage(message);
-
-    // Clear the text in the input fields:
-    $username.val('');
-    $text.val('');
-    $roomname.val('');
-  });
-
-  var $roomSelector = $('#room-selector');
-  $roomSelector.on('change', function() {
-    // console.log(this.value); // or $(this).val()
-    getMessages();
-  });
-
-  // Allow user to befriend other users by clicking on their user name
-  // var $username = $('.username');
-  // $username.on('click', function() {
-  //   console.log($username.val());
-  // });
-
-  $('#chats').on('click', '.username', function() {    
-    friends.push($(this).text());
-    getMessages();
-  });
-
-  getMessages();
-
-});
-
+app.init();
 
 
 
